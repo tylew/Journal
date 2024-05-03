1b87bac568ef544ebff47e901b9f86887eb2f402
Tyler L. (desktop) <69596889+tylew@users.noreply.github.com>
2024-05-02 22:23:21 -0700

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Tyler Lewis on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
